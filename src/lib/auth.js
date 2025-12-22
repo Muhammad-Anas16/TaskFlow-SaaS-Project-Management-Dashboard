47ac636996e57a8aec7ba21c65989a4b88fed0a9
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-12-22 13:44:04 +0500

    update a little

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,18 +6,20 @@ const client = new MongoClient(process.env.DATABASE_URL);
 const db = client.db();
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db, { client }),
-    emailAndPassword: {
-        enabled: true,
-    },
-    //   socialProviders: {
-    //     google: {
-    //       clientId: process.env.GOOGLE_CLIENT_ID,
-    //       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    //     },
-    //     github: {
-    //       clientId: process.env.GITHUB_CLIENT_ID,
-    //       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    //     },
-    //   },
+	database: mongodbAdapter(db, { client }),
+	emailAndPassword: {
+		enabled: true,
+	},
 });
+
+
+//   socialProviders: {
+//     google: {
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     },
+//     github: {
+//       clientId: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//     },
+//   },
