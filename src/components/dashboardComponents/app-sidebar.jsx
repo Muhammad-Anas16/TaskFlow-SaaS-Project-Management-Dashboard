54f544cf7d424a353b0e8bdeb3273c85afe33fe0
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-12-25 14:12:11 +0500

    update the dashboard UI

--- src/components/dashboardComponents/app-sidebar.jsx
+++ src/components/dashboardComponents/app-sidebar.jsx
@@ -16,6 +16,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "../ui/sidebar";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 const items = [
   { id: "dashboard", label: "Dashboard", icon: LayoutDashboard },
@@ -27,22 +28,22 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar className="border-none bg-[#081f17]">
+    <Sidebar className="border-r border-emerald-500/20 bg-[#02110c] text-emerald-300 w-64">
       {/* LOGO */}
-      <SidebarHeader className="px-6 py-6">
+      <SidebarHeader className="px-6 py-6 bg-[#04140F] border-b border-emerald-500/20">
         <div className="flex items-center gap-3">
           <div className="h-9 w-9 rounded-full bg-emerald-400 flex items-center justify-center font-bold text-black">
-            ✓
+            <IoMdCheckmarkCircleOutline size={20} />
           </div>
           <div>
-            <p className="font-semibold">TaskFlow</p>
+            <h1 className="text-white text-2xl font-bold">TaskFlow</h1>
             <p className="text-xs text-emerald-300/70">Manage better</p>
           </div>
         </div>
       </SidebarHeader>
 
       {/* MENU */}
-      <SidebarContent className="px-2">
+      <SidebarContent className="px-2 py-4 flex-1 bg-[#04140F] border-b border-emerald-500/20">
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.id}>
@@ -59,7 +60,9 @@ export function AppSidebar() {
                   text-emerald-100
                   hover:bg-emerald-500/10
                   hover:text-white
-                  data-[active=true]:bg-emerald-500/20
+                  data-[active=true]:bg-emerald-500/20 data-[active=true]:text-white
+                  flex items-center gap-3
+                  w-full text-left cursor-pointer
                 "
               >
                 <item.icon className="h-4 w-4" />
@@ -71,7 +74,7 @@ export function AppSidebar() {
       </SidebarContent>
 
       {/* USER */}
-      <SidebarFooter className="px-6 py-4">
+      <SidebarFooter className="px-6 py-4 bg-[#04140F] border-b border-emerald-500/20">
         <div className="flex items-center gap-3">
           <div className="h-9 w-9 rounded-full bg-black flex items-center justify-center">
             A
